14d3553013812f559c1373a46233f526c6c64066
David Konsumer <konsumer@jetboystudio.com>
2019-03-08 05:09:37 -0800

    tidyed up everything, but insta droped a bunch of fields

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ This is a utility to grab referenced accounts that fit certain criteria from ins
 npm i    # install dependencies
 npm test # run the script and output CSV to stdout.
 ```
+
+Add your rules & influencers to text files in `settings/`, 1 item per line:
+
+```
+influencers     - people who link to the instagram users you want info from
+rules_notprefix - things that should not appear before a username
+rules_preceding - things that should appear before a username
+rules_username  - things that if found in username, match
+```
+
+Make sure you set `INSTAGRAM_USER` and `INSTAGRAM_PASSWORD` environment variables.
--- test.js
+++ test.js
@@ -1,59 +1,137 @@
 const puppeteer = require('puppeteer')
-
-const config = require('./config.json')
-const { login, getUser } = require('./insta')
-const { csv } = require('./utils')
+const pThrottle = require('p-throttle')
+const json2csv = require('json2csv').parse
+const { readFile } = require('fs')
 
 const { INSTAGRAM_USER, INSTAGRAM_PASSWORD } = process.env
 
-// build regex from config
-const getUsersRegex = (precedingRules, usernameRules) => {
-  let r = usernameRules.map(u => `@([a-z0-9._]*${u}[a-z0-9._]*)`).join('|')
-  r += '|' + precedingRules.map(p => `${p} @([a-z0-9._]+)`).join('|')
-  return new RegExp(r, 'ig')
+// get a 1-per-line text file list as an array
+const getList = async (name) => new Promise((resolve, reject) => {
+  readFile(`${__dirname}/settings/${name}.txt`, (err, results) => {
+    if (err) reject(err)
+    resolve(results.toString().split('\n').map(i => i.trim()).filter(i => i && i !== ''))
+  })
+})
+
+// get a single user from instagram
+const getUser = pThrottle(async (username, browser) => {
+  try {
+    const page = await browser.newPage()
+    const u = await page.goto(`https://www.instagram.com/${username.replace(/\.$/, '')}/?__a=1`)
+    if (u.status() !== 200) {
+      console.error(`${username}: bad status`)
+      return
+    }
+    const text = await u.text()
+    const user = JSON.parse(text)
+    // .then(r => r.json())
+    if (user && user.graphql && user.graphql.user) {
+      return user.graphql.user
+    } else {
+      console.error(`${username}: bad JSON shape`)
+    }
+  } catch (e) {
+    console.error(`${username}: ${e.message}`)
+  }
+}, 2, 1000)
+
+// login to instagram
+const login = async (username, password, browser) => {
+  const page = await browser.newPage()
+  await page.goto('https://www.instagram.com/accounts/login/')
+  await page.waitFor(2500)
+  await page.click('input[type="text"]')
+  await page.keyboard.type(username)
+  await page.click('input[type="password"]')
+  await page.keyboard.type(password)
+  await page.click('button[type="submit"]')
+  await page.waitForNavigation()
 }
+
+// build the user-finding regex from positive rules
+const getUsersRegex = async () => new RegExp((await getList('rules_username'))
+  .map(u => `@([a-z0-9._]*${u}[a-z0-9._]*)`)
+  .join('|') + '|' +
+  (await getList('rules_preceding'))
+    .map(p => `${p} @([a-z0-9._]+)`)
+    .join('|'),
+'ig')
+
+// build the no-prefix rules regex
+const getNotRegex = async () => new RegExp((await getList('rules_notprefix')).map(p => `${p} @([a-z0-9._]+)`).join('|'), 'ig')
 
 // simply find all matches & remove duplicates
 const find = (regex, string) => {
   let m
-  let out = []
+  const out = []
   while ((m = regex.exec(string)) !== null) {
-    out = out.concat(out, m.slice(1))
+    out.push(m[1])
   }
   return out.filter((v, i, a) => i && a.indexOf(v) === i)
 }
 
-;(async () => {
+const fields = [
+  'FirstName',
+  'LastName',
+  'Organization',
+  'Phone',
+  'Email',
+  'Website',
+  'Street',
+  'City',
+  'State',
+  'PostalCode',
+  'Country',
+  'Source Detail',
+  'Account Status',
+  'Account Type'
+]
+
+const run = async () => {
   const browser = await puppeteer.launch()
   await login(INSTAGRAM_USER, INSTAGRAM_PASSWORD, browser)
-  const source = await Promise.all(config.influencers.map(async username => getUser(username, browser)))
-  const userRegex = getUsersRegex(config.rules.preceding, config.rules.username)
-
-  // find all interesting usernames in text of message
-  const users = new Set()
-  source.forEach(influencer => {
-    const text = influencer.edge_owner_to_timeline_media.edges.map(e => e.node.edge_media_to_caption.edges[0].node.text).join('\n')
-    find(userRegex, text).forEach(u => users.add(u))
-  })
-
-  const data = (await Promise.all([...users].map(async username => {
-    try {
-      const user = await getUser(username, browser)
-      const { external_url, full_name, business_phone_number, business_email, business_address_json, connected_fb_page } = user
-      const business_address = JSON.parse(business_address_json)
-      return {
-        url: external_url,
-        phone: business_phone_number,
-        email: business_email,
-        ...business_address,
-        instagram: username,
-        facebook: connected_fb_page,
-        full_name
-      }
-    } catch (e) {
-      console.error(username, e.message)
+  const influencers = (await Promise.all((await getList('influencers')).map(u => getUser(u, browser)))).filter(u => u)
+  const rPositive = await getUsersRegex()
+  const rNegative = await getNotRegex()
+  let text = ''
+  influencers.forEach(influencer => {
+    if (influencer && influencer.edge_owner_to_timeline_media && influencer.edge_owner_to_timeline_media.edges) {
+      influencer.edge_owner_to_timeline_media.edges.forEach(e => {
+        if (e && e.node && e.node.edge_media_to_caption && e.node.edge_media_to_caption.edges && e.node.edge_media_to_caption.edges[0] && e.node.edge_media_to_caption.edges[0].node && e.node.edge_media_to_caption.edges[0].node.text) {
+          text += '\n' + e.node.edge_media_to_caption.edges[0].node.text
+        }
+      })
     }
-  }))).filter(u => u)
+  })
+  const data = (await Promise.all(find(rPositive, text.replace(rNegative, ''))
+    .filter((u, i, a) => u && a.indexOf(u) === i)
+    .map(u => getUser(u, browser))
+  ))
+    .filter(u => u)
+    .map(user => {
+      console.log(JSON.stringify(user, null, 2))
+      const [FirstName, ...LastName] = user.full_name.split(' ')
+      const businessAddress = user.business_address_json ? JSON.parse(user.business_address_json) : { city_name: ', ' }
+      const [ City, State ] = businessAddress.city_name.split(', ')
+      console.error(Object.keys(user))
+      return {
+        FirstName,
+        LastName: LastName.join(' '),
+        Organization: user.username,
+        Phone: user.business_phone_number,
+        Email: user.business_email,
+        Website: user.external_url,
+        Street: businessAddress.street_address,
+        City,
+        State,
+        PostalCode: businessAddress.zip_code,
+        Country: businessAddress.country_code,
+        'Source Detail': 'Instagram',
+        'Account Status': 'Pending Customer',
+        'Account Type': 'Trade'
+      }
+    })
   browser.close()
-  console.log(csv(data, ['url', 'phone', 'email', 'instagram', 'facebook', 'full_name', 'street_address', 'zip_code', 'city_name', 'region_name', 'country_code']))
-})()
+  // console.log(json2csv(data, { fields }))
+}
+run()
